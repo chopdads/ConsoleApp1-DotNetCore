75cc012c56863b919a4b400b538b620eb417533d
Deepesh <chopdadeepesh@gmail.com>
2020-03-25 17:57:09 +0530

    Add project files.

---
+++ ConsoleApp1-DotNetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1_DotNetCore
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Hello Deepesh");
+            Console.ReadLine();
+        }
+    }
+}
